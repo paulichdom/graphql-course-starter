3a7f9fba89e91438a7e4e616fe2987b35f58c174
Domagoj <paulic.domagoj@gmail.com>
2024-12-20 13:02:34 +0100

    setup gql server

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,40 @@
-import express from "express";
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+const server = new ApolloServer({
+  typeDefs: `
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => 'Hello GraphQL',
+    },
+  },
+});
+
 async function startServer() {
+  await server.start();
+
+  app.use(
+    '/graphql',
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    }),
+    cookieParser(),
+    express.json(),
+    expressMiddleware(server)
+  );
+
   app.listen(4000, () => {
-    console.log("Server is running on http://localhost:4000/graphql");
+    console.log('Server is running on http://localhost:4000/graphql');
   });
 }
 
