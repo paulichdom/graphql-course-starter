02f62e35aad78b798ab371c954277e3d40d66f84
paulichdom <paulic.domagoj@gmail.com>
2024-12-26 17:19:37 +0100

    implement cancel application 19

--- src/routes/Profile/queries.generated.tsx
+++ src/routes/Profile/queries.generated.tsx
@@ -8,6 +8,13 @@ export type ProfileQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 export type ProfileQuery = { __typename?: 'Query', me?: { __typename?: 'User', id: string, name: string, email: string, appliedJobs?: Array<{ __typename?: 'Job', id: string, title: string, location: string, type: Types.JobType, remote: boolean, salary: number, createdAt: Date, company: { __typename?: 'Company', id: string, name: string } }> | null } | null };
 
+export type CancelApplicationMutationVariables = Types.Exact<{
+  input: Types.CancelApplicationInput;
+}>;
+
+
+export type CancelApplicationMutation = { __typename?: 'Mutation', cancelApplication: boolean };
+
 
 export const ProfileDocument = gql`
     query Profile {
@@ -63,3 +70,34 @@ export type ProfileQueryHookResult = ReturnType<typeof useProfileQuery>;
 export type ProfileLazyQueryHookResult = ReturnType<typeof useProfileLazyQuery>;
 export type ProfileSuspenseQueryHookResult = ReturnType<typeof useProfileSuspenseQuery>;
 export type ProfileQueryResult = Apollo.QueryResult<ProfileQuery, ProfileQueryVariables>;
+export const CancelApplicationDocument = gql`
+    mutation CancelApplication($input: CancelApplicationInput!) {
+  cancelApplication(input: $input)
+}
+    `;
+export type CancelApplicationMutationFn = Apollo.MutationFunction<CancelApplicationMutation, CancelApplicationMutationVariables>;
+
+/**
+ * __useCancelApplicationMutation__
+ *
+ * To run a mutation, you first call `useCancelApplicationMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCancelApplicationMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [cancelApplicationMutation, { data, loading, error }] = useCancelApplicationMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useCancelApplicationMutation(baseOptions?: Apollo.MutationHookOptions<CancelApplicationMutation, CancelApplicationMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CancelApplicationMutation, CancelApplicationMutationVariables>(CancelApplicationDocument, options);
+      }
+export type CancelApplicationMutationHookResult = ReturnType<typeof useCancelApplicationMutation>;
+export type CancelApplicationMutationResult = Apollo.MutationResult<CancelApplicationMutation>;
+export type CancelApplicationMutationOptions = Apollo.BaseMutationOptions<CancelApplicationMutation, CancelApplicationMutationVariables>;
--- src/routes/Profile/useCancelApplication.ts
+++ src/routes/Profile/useCancelApplication.ts
@@ -1,7 +1,24 @@
+import client from "../../client";
+import { useCancelApplicationMutation } from "./queries.generated";
+
 const useCancelApplication = () => {
+  const [cancelApplication, {loading}] = useCancelApplicationMutation()
   return {
-    cancelApplication: async (id: string) => {},
-    isLoading: false,
+    cancelApplication: async (id: string) => {
+      await cancelApplication({
+        variables: {input: {id}},
+        refetchQueries: ['Profile'],
+        onCompleted: () => {
+          client.cache.modify({
+            id: `Job:${id}`,
+            fields: {
+              isApplied: () => false
+            }
+          })
+        }
+      })
+    },
+    isLoading: loading,
   };
 };
 
